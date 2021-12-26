613d1fb8d9ca36f7ffbf9f22a8ebac76883cc4b0
Matthew Garcia <matthew27garcia@gmail.com>
2021-12-26 01:40:26 -0600

    added some const's

---
+++ game.js
@@ -0,0 +1,6 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
